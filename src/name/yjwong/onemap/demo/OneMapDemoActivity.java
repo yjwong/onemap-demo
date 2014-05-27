64f2df78dd7640b440b818cf40714f2212905470
Wong Yong Jie <yjwong92@gmail.com>
2014-05-28 03:29:13 +0800

    Show how to center the map at a certain coordinate and set the scaling of the map.

--- src/name/yjwong/onemap/demo/OneMapDemoActivity.java
+++ src/name/yjwong/onemap/demo/OneMapDemoActivity.java
@@ -2,11 +2,14 @@ package name.yjwong.onemap.demo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.esri.android.map.Layer;
 import com.esri.android.map.MapView;
 import com.esri.android.map.ags.ArcGISTiledMapServiceLayer;
+import com.esri.android.map.event.OnStatusChangedListener;
 import com.esri.android.runtime.ArcGISRuntime;
-
+import com.esri.core.geometry.Point;
 
 public class OneMapDemoActivity extends Activity {
 	
@@ -21,10 +24,25 @@ public class OneMapDemoActivity extends Activity {
         // Retrieve the map from XML layout.
 		mMapView = (MapView) findViewById(R.id.map);
 		
-		// Add dynamic layer to MapView.
-		mMapView.addLayer(new ArcGISTiledMapServiceLayer(
+		// Create the layer.
+		final Layer oneMapLayer = new ArcGISTiledMapServiceLayer(
 				"http://e1.onemap.sg/arcgis/rest/services/BASEMAP/MapServer"
-		));
+		);
+		
+		// We want to know when the map layer is loaded.
+		mMapView.setOnStatusChangedListener(new OnStatusChangedListener() {
+			private static final long serialVersionUID = 1L;
+
+			public void onStatusChanged(Object source, STATUS status) {
+				if (source == oneMapLayer && status == STATUS.LAYER_LOADED) {
+					mMapView.centerAt(1.286389, 103.79, false);
+					mMapView.setScale(1500, false);
+				}
+			}
+		});
+		
+		// Add dynamic layer to MapView.
+		mMapView.addLayer(oneMapLayer);
 		
 		// Set the client ID for Runtime SDK Licensing.
 		// Replace this with your own.
